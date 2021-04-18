d7c37a41194c4ca4ed1e6ee7906ea1ded0836bca
Harun Doğdu <Harundogdu06@gmail.com>
2021-04-19 00:22:34 +0300

    Adding items to the list.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,39 +14,32 @@ const ProductController = (() => {
     }
 
     const data = {
-        products: [{
-                id: 0,
-                name: 'Monitör',
-                price: '1000'
-            },
-            {
-                id: 1,
-                name: 'Klavye',
-                price: '200'
-            },
-            {
-                id: 2,
-                name: 'Ram',
-                price: '400'
-            },
-            {
-                id: 3,
-                name: 'Mouse',
-                price: '50'
-            }
-        ],
+        products: [],
         selectedProduct: null,
         totalPrice: 0
     }
 
     //public
-
     return {
         getProducts: () => {
             return data.products
         },
         getData: () => {
             return data
+        },
+        addProduct: function (name, price) {
+            let id;
+
+            if (data.products.length > 0) {
+                id = data.products.length;
+            } else {
+                id = 0;
+            }
+
+            const newProduct = new Products(id, name, parseFloat(price));
+            data.products.push(newProduct);
+
+            return newProduct;
         }
     }
 
@@ -56,7 +49,13 @@ const ProductController = (() => {
 const UIController = (() => {
     //private
     const Selectors = {
-        productList: '#products-list'
+        productList: '#products-list',
+        productName: '#productName',
+        productPrice: '#productPrice',
+        buttonAdd: '.btnAdd',
+        productCard: '#productCard',
+        totalCard: '#totalCard'
+
     }
 
     //public
@@ -65,14 +64,14 @@ const UIController = (() => {
             let html = "";
 
             products.forEach(product => {
-               html +=
+                html +=
                     `
                 <tr>
                     <td>${product.id}</td>
                     <td>${product.name}</td>
                     <td>${product.price}</td>
                     <td class="text-right">
-                        <button id="btnEdit" class="btn btn-warning btn-sm" type="submit">
+                        <button title="Edit Product" class="btn btn-warning btn-sm btnEdit" type="submit">
                             <i class="fa fa-edit" aria-hidden="true"></i>
                         </button>
                     </td>
@@ -81,6 +80,37 @@ const UIController = (() => {
             });
 
             document.querySelector(Selectors.productList).innerHTML = html;
+        },
+        getSelectors: () => {
+            return Selectors;
+        },
+        addProduct: function (product) {
+            document.querySelector(Selectors.productCard).style.display = 'block';
+            document.querySelector(Selectors.totalCard).style.display = 'block';
+            var item =
+                `
+                <tr>
+                    <td>${product.id}</td>
+                    <td>${product.name}</td>
+                    <td>${product.price}</td>
+                    <td class="text-right">
+                        <button title="Edit Product" class="btn btn-warning btn-sm btnEdit" type="submit">
+                            <i class="fa fa-edit" aria-hidden="true"></i>
+                        </button>
+                    </td>
+                </tr>
+                `;
+
+            document.querySelector(Selectors.productList).innerHTML += item;
+
+        },
+        clearInputs: () => {
+            document.querySelector(Selectors.productName).value = "";
+            document.querySelector(Selectors.productPrice).value = "";
+        },
+        hideCard: () => {
+            document.querySelector(Selectors.productCard).style.display = 'none';
+            document.querySelector(Selectors.totalCard).style.display = 'none';
         }
     }
 
@@ -89,11 +119,44 @@ const UIController = (() => {
 // App Controller
 const AppController = ((ProductCtrl, UICtrl) => {
 
+    const UISelector = UICtrl.getSelectors();
+
+    //load event listeners
+    const loadEventListeners = () => {
+        document.querySelector(UISelector.buttonAdd).addEventListener('click', productAddSubmit);
+    }
+
+    const productAddSubmit = ((e) => {
+
+        const productName = document.querySelector(UISelector.productName).value;
+        const productPrice = document.querySelector(UISelector.productPrice).value;
+
+        if (productName !== '' && productPrice !== '') {
+            // add product
+            const newProduct = ProductCtrl.addProduct(productName, productPrice);
+
+            // add item to list
+            UICtrl.addProduct(newProduct);
+
+            // clear inputs
+            UICtrl.clearInputs();
+        } else {
+            alert('Please fill in the relevant fields.');
+        }
+
+        e.preventDefault();
+    });
+
     return {
         init: () => {
             console.log('starting app..');
-            let products = ProductCtrl.getProducts();
-            UICtrl.createProductList(products);
+            const products = ProductCtrl.getProducts();
+            if (products.length == 0) {
+                UICtrl.hideCard();
+            } else {
+                UICtrl.createProductList(products);
+            }
+            loadEventListeners();
         }
     }
 
